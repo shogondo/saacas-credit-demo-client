adcf5732b4de2d27d13ddd6dcf3e8c86e2349721
Shou Takenaka <sasaminn@gmail.com>
2015-04-12 16:49:15 +0900

    Change theme to Holo Light

--- app/src/main/java/shou/saacas/demo/client/MainActivity.java
+++ app/src/main/java/shou/saacas/demo/client/MainActivity.java
@@ -1,21 +1,21 @@
 package shou.saacas.demo.client;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
