e7b06f65b454daa535d5252a2a9aa04e6704d7ee
Shou Takenaka <sasaminn@gmail.com>
2015-04-12 16:56:05 +0900

    Extract inner fragment to EntryAmountFragment class

---
+++ app/src/main/java/shou/saacas/demo/client/EntryAmountFragment.java
@@ -0,0 +1,14 @@
+package shou.saacas.demo.client;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EntryAmountFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.entry_amount, container, false);
+    }
+}
--- app/src/main/java/shou/saacas/demo/client/MainActivity.java
+++ app/src/main/java/shou/saacas/demo/client/MainActivity.java
@@ -1,12 +1,7 @@
 package shou.saacas.demo.client;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 
 public class MainActivity extends Activity {
 
@@ -16,23 +11,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new EntryAmountFragment())
                     .commit();
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.entry_amount, container, false);
         }
     }
 }
