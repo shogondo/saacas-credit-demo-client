969ba9e4f8d83ae55398e2367e8632f3a66e2108
Shou Takenaka <sasaminn@gmail.com>
2015-04-12 17:23:57 +0900

    Allow complete payment view to transit to entry amount view

--- app/src/main/java/shou/saacas/demo/client/CompletePaymentFragment.java
+++ app/src/main/java/shou/saacas/demo/client/CompletePaymentFragment.java
@@ -5,12 +5,26 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CompletePaymentFragment extends Fragment {
+    private Button retryButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.complete_payment, container, false);
+        View view = inflater.inflate(R.layout.complete_payment, container, false);
+
+        retryButton = (Button) view.findViewById(R.id.retry_button);
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, new EntryAmountFragment())
+                        .commit();
+            }
+        });
+
+        return view;
     }
-
-
 }
